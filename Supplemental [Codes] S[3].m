b04e3570c3e423e04be4f759af869344bf7d8c9f
Jianjun158 <115689981+Jianjun158@users.noreply.github.com>
2022-12-08 13:48:59 +0800

    Update Supplemental [Codes] S[3].m

--- Supplemental [Codes] S[3].m
+++ Supplemental [Codes] S[3].m
@@ -1,17 +1,12 @@
-function [LSCM] = LSCM(PO,PN)
-% LSCM Method
+function [LICM] = LICM(PO,PN)
+%LICM Method
 pi=3.141592653;
-N=2000;
+N=1500;
 Ex1=PO(1);
 En1=PO(2);
 He1=PO(3);
 Ex2=PN(1);
 En2=PN(2);
 He2=PN(3);
-l0=max(0,(min(Ex1+3*En1,Ex2+3*En2)-max(Ex1-3*En1,Ex2-3*En2)));
-la=6*(En1+En2);
-sl=(2*l0/la)*exp(-abs(Ex1-Ex2)/10);
-s2=(min(En1*exp(3*He1/En1),En2*exp(3*He2/En2))/max(En1*exp(3*He1/En1),En2*exp(3*He2/En2)));
-LSCM=sl*s2;
+LICM=(Ex1*Ex2+En1*En2+He1*He2)/(sqrt(Ex1^2+En1^2+He1^2)*sqrt(Ex2^2+En2^2+He2^2))
 end
-
